c49b802e6e5e69f5f792f1e06807a0057f8d7f33
Ishan Mehta <73947463+IshanMehta115@users.noreply.github.com>
2021-01-18 13:50:29 +0530

    Update game.py
    Added an option to select difficulty levels. When set to Hard level the computer uses the Minimax Algorithm to find an optimal move.

--- game.py
+++ game.py
@@ -15,8 +15,8 @@ bg_img = pygame.image.load(os.path.join(base_path,"bg.png"))
 
 game_name_font = pygame.font.SysFont("comicsans",100,False,False)
 button_font = pygame.font.SysFont("comicsans",50,False,False)
-marker_font = pygame.font.SysFont('comicsans',30,True,False)
-score_font = pygame.font.SysFont('comicsans',28,True,False)
+score_font = pygame.font.SysFont('comicsans',40,False,False)
+end_game_font = pygame.font.SysFont('comicsans',60,False,False)
 
 game_name = "Tic-Tac-Toe"
 game_name_text = ''
@@ -32,7 +32,7 @@ computer_score = 0
 
 buttons = []
 optionButtons = []
-button_number = 2
+button_number = 3
 buttonWidth=200
 buttonHeight=50
 buttonGap=buttonHeight+30
@@ -46,6 +46,8 @@ number=0
 dir=''
 won=False
 strike_fraction = 0
+levels = ['Easy','Hard']
+level = 0
 
 new_screen_ready=False
 screen_selected='main menu'
@@ -93,6 +95,7 @@ def set_main_menu():
     
 
     buttons.append(button(play_game,'DarkBlue','White',button_font,'normal'))
+    buttons.append(button('Level : '+levels[level],'DarkBlue','White',button_font,'normal'))
     buttons.append(button(exit_game,'DarkBlue','White',button_font,'normal'))
     for i in range(button_number):
         buttons[i].set_rect(pygame.Rect(screenWidth/2-buttonWidth/2,200+i*buttonGap,buttonWidth,buttonHeight))
@@ -128,7 +131,7 @@ def show_options():
         optionButtons[i].display()
 
 def check_click():
-    global screen_selected,new_screen_ready
+    global screen_selected,new_screen_ready,level
     mouse_button1, mouse_button2, mouse_button3 = pygame.mouse.get_pressed()
     mouseX,mouseY = pygame.mouse.get_pos()
     if(not mouse_button1):
@@ -136,8 +139,13 @@ def check_click():
     if(screen_selected=='main menu'):
         for i in buttons:
             if (i.rect.x <= mouseX <= i.rect.x+i.rect.width) and (i.rect.y <= mouseY <= i.rect.y+i.rect.height):
-                screen_selected=i.text
-                new_screen_ready=False
+                if('Level' in i.text):
+                    level=1-level
+                    i.text = 'Level : '+levels[level]
+                    new_screen_ready=False
+                else:
+                    screen_selected=i.text
+                    new_screen_ready=False
     elif(screen_selected=='options'):
         for i in optionButtons:
             if (i.rect.x <= mouseX <= i.rect.x+i.rect.width) and (i.rect.y <= mouseY <= i.rect.y+i.rect.height):
@@ -179,9 +187,70 @@ def check_win():
 
     if game_grid[2][0]==game_grid[1][1] and game_grid[1][1]==game_grid[0][2]:
         return 2,'dia',True,game_grid[1][1]    
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if game_grid[i][j]==human_player or game_grid[i][j]==computer_player:
+                count+=1
+    if(count==9):
+        return 0,'',True,'tie'
     return 0,'',False,''
         
+def randomMove():
+    i=random.randint(0,2)
+    j=random.randint(0,2)
+    while(not position_available(i,j)):
+        i=random.randint(0,2)
+        j=random.randint(0,2)
+    return i,j
+def minimax(isMaximizingTurn):
+    number,dir,won,player_type = check_win()
+    if(won):
+        if player_type==computer_player:
+            return 1
+        elif player_type==human_player:
+            return -1
+        elif player_type=='tie':
+            return 0
+    if(isMaximizingTurn):
+        bestScore = -100
+        for i in range(3):
+            for j in range(3):
+                if game_grid[i][j]!=human_player and game_grid[i][j]!=computer_player:
+                    original_value = game_grid[i][j]
+                    game_grid[i][j]=computer_player
+                    score = minimax(False)
+                    game_grid[i][j]=original_value
+                    if(bestScore<score):
+                        bestScore=score
+        return bestScore
+    else:
+        bestScore = 100
+        for i in range(3):
+            for j in range(3):
+                if game_grid[i][j]!=human_player and game_grid[i][j]!=computer_player:
+                    original_value = game_grid[i][j]
+                    game_grid[i][j]=human_player
+                    score = minimax(True)
+                    game_grid[i][j]=original_value
+                    if(score<bestScore):
+                        bestScore=score
+        return bestScore
 
+def MiniMax_Algo():
+    positions = ''
+    best_score = -100
+    for i in range(3):
+        for j in range(3):
+            if game_grid[i][j]!=human_player and game_grid[i][j]!=computer_player:
+                original_value = game_grid[i][j]
+                game_grid[i][j]=computer_player
+                score = minimax(False)
+                game_grid[i][j]=original_value
+                if(best_score<score):
+                    best_score=score
+                    positions = [i,j]
+    return positions[0],positions[1]
 def main_loop():
     run = True
     global turn,time,screen_selected,number,dir,won,new_screen_ready,moves,human_score,computer_score,computer_player,human_player
@@ -205,9 +274,9 @@ def main_loop():
                     pygame.draw.line(window,(0,0,0),(3*unitLength,unitLength),(3*unitLength,screenHeight-unitLength),line_width)
                     pygame.draw.line(window,(0,0,0),(unitLength,2*unitLength),(screenWidth-unitLength,2*unitLength),line_width)
                     pygame.draw.line(window,(0,0,0),(unitLength,3*unitLength),(screenWidth-unitLength,3*unitLength),line_width)
-                    human_score_text = score_font.render('You - '+(str)(human_score),True,pygame.Color('DarkBlue'))
+                    human_score_text = score_font.render('You : '+(str)(human_score),True,pygame.Color('DarkBlue'))
                     window.blit(human_score_text,(screenWidth-human_score_text.get_width()-10,20))
-                    computer_score_text = score_font.render('Computer - '+(str)(computer_score),True,pygame.Color('DarkBlue'))
+                    computer_score_text = score_font.render('Computer : '+(str)(computer_score),True,pygame.Color('DarkBlue'))
                     window.blit(computer_score_text,(10,20))
 
                     mousex,mousey = pygame.mouse.get_pos()
@@ -231,12 +300,12 @@ def main_loop():
                                     gap = 10
                                     pygame.draw.rect(window,(179, 179, 255),(unitLength*(mousex+1)+gap,unitLength*(mousey+1)+gap,unitLength-2*gap,unitLength-2*gap))
                     elif(turn==computer_player and moves<9 and time<0):
-                        mousex=random.randint(0,2)
-                        mousey=random.randint(0,2)
-                        while(not position_available(mousey,mousex)):
-                            mousex=random.randint(0,2)
-                            mousey=random.randint(0,2)
-                        game_grid[mousey][mousex]=computer_player
+                        nextBestMove=0
+                        if(level==0):
+                            nextBestMove = randomMove()
+                        else:
+                            nextBestMove = MiniMax_Algo()
+                        game_grid[nextBestMove[0]][nextBestMove[1]]=computer_player
                         moves+=1
                         turn=human_player
                     
@@ -252,7 +321,7 @@ def main_loop():
                         pygame.time.delay(500)
                         if(player_type==human_player):
                             human_score+=1
-                        else:
+                        elif(player_type==computer_player):
                             computer_score+=1
                 else:
                     mouseBtn1,mouseBtn2,mouseBtn3 = pygame.mouse.get_pressed()
@@ -275,10 +344,20 @@ def main_loop():
             pygame.draw.line(window,(0,0,0),(3*unitLength,unitLength),(3*unitLength,screenHeight-unitLength),line_width)
             pygame.draw.line(window,(0,0,0),(unitLength,2*unitLength),(screenWidth-unitLength,2*unitLength),line_width)
             pygame.draw.line(window,(0,0,0),(unitLength,3*unitLength),(screenWidth-unitLength,3*unitLength),line_width)
-            human_score_text = score_font.render('You - '+(str)(human_score),True,pygame.Color('DarkBlue'))
+            human_score_text = score_font.render('You : '+(str)(human_score),True,pygame.Color('DarkBlue'))
             window.blit(human_score_text,(screenWidth-human_score_text.get_width()-10,20))
-            computer_score_text = score_font.render('Computer - '+(str)(computer_score),True,pygame.Color('DarkBlue'))
+            computer_score_text = score_font.render('Computer : '+(str)(computer_score),True,pygame.Color('DarkBlue'))
             window.blit(computer_score_text,(10,20))
+
+            end_game = ''
+            if(player_type==human_player):
+                end_game = 'You Won'
+            elif(player_type==computer_player):
+                end_game = 'You Lose'
+            elif(player_type=='tie'):
+                end_game = 'Draw'
+            winner_text = end_game_font.render(end_game,True,pygame.Color('DarkBlue'))
+            window.blit(winner_text,(screenWidth/2-winner_text.get_width()/2,screenHeight-winner_text.get_height()-30))
            
             for i in range(3):
                 for j in range(3):
